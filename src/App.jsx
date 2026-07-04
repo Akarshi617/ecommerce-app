2a4dd56f37bbcf86aae9ba48ceefb1bee2356371
Akarshi617 <agrahariakarshi786@gmail.com>
2026-07-04 18:06:18 +0530

    Remove Navbar import and component usage

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ import Shop from "./pages/Shop";
 import ProductDetail from "./pages/ProductDetail";
 import Login from "./pages/Login";
 import Checkout from "./pages/Checkout";
-import Navbar from "./components/Navbar";
-// ... routes ke upar
-<Navbar />
+
 function App() {
   return (
     <AuthProvider>
